2ae7b935d745f162fa1dd4a63658b91e9d6045f8
rpźbass <ronaldop.tech@gmail.com>
2021-05-04 17:24:52 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/wkvendas/WkvendasApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.wkvendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WkvendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WkvendasApplication.class, args);
+	}
+
+}
